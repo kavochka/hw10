e9d77879335ddf9401453f680967b456f4f5f791
kavochka <ana.bane@yandex.ru>
2024-06-16 16:27:15 +0300

    hw done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("task 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.printf("Ф.И.О. сотрудника - %s.%n", fullName);
+
+        System.out.println("task 2");
+        System.out.printf("Данные Ф.И.О. сотрудника для заполнения отчета - %s.%n", fullName.toUpperCase());
+
+        System.out.println("task 3");
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "e");
+        System.out.printf("Данные Ф.И.О. сотрудника - %s.%n", fullName);
+
     }
 }
